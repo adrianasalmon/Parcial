93c6ad26b66e5c76a5b7a6e40262940afc8936e8
adrianasalmon <a2019116341@estudiantes.upsa.edu.bo>
2020-09-19 13:41:09 -0400

    Creacion de Unite Test

---
+++ ParcialAdrianaSalmon/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialAdrianaSalmon.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetCountry()
+        {
+            //Arrange
+            CountriesController countriesController = new CountriesController();
+            //Act
+            var Lista = countriesController.GetCountries();
+            //Asser
+            Assert.IsNotNull(Lista);
+        }
+    }
+}
